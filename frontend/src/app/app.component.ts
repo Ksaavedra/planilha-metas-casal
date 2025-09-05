6ee02bec1c7e1e61e5db48093aa2edbf89366144
Ksaavedra <kellymichelesaavedra@gmail.com>
2025-09-04 22:35:17 -0300

    fix: Corrigir import do AppHeaderComponent no app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AppHeaderComponent } from './shared';
+import { AppHeaderComponent } from './shared/components/app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
